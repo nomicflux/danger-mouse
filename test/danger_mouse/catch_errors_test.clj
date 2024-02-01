bc7dedd6a25038d29f488ba5b611c50097f4d041
Michael A. <nomicflux@gmail.com>
2024-02-01 17:43:59 -0600

    indenting

--- test/danger_mouse/catch_errors_test.clj
+++ test/danger_mouse/catch_errors_test.clj
@@ -5,8 +5,8 @@
 (defn throw-on-even
   [n]
   (if (even? n)
-        (throw (ex-info "Even!" {:n n}))
-        n))
+    (throw (ex-info "Even!" {:n n}))
+    n))
 
 (deftest catch-errors-test
   (is (= {:result [1 2 3 4 5 6 7 8 9 10]
@@ -15,20 +15,20 @@
                     conj []
                     (range 0 10))))
   (is (= {:result [1 3 5 7 9]
-           :errors
+          :errors
           [{:error-msg "Even!" :input 0}
            {:error-msg "Even!" :input 2}
            {:error-msg "Even!" :input 4}
            {:error-msg "Even!" :input 6}
            {:error-msg "Even!" :input 8}]}
          (-> (transduce (comp sut/catch-errors (map throw-on-even))
-                     conj []
-                     (range 0 10))
+                        conj []
+                        (range 0 10))
              (update :errors (partial map #(dissoc % :error)))))))
 
 (deftest catch-errors->-test
   (is (= {:result [1 3 5 7 9]
-           :errors
+          :errors
           [{:error-msg "Even!" :input 0}
            {:error-msg "Even!" :input 2}
            {:error-msg "Even!" :input 4}
@@ -39,7 +39,7 @@
 
 (deftest transduce->-test
   (is (= {:result 25
-           :errors
+          :errors
           [{:error-msg "Even!" :input 0}
            {:error-msg "Even!" :input 2}
            {:error-msg "Even!" :input 4}
